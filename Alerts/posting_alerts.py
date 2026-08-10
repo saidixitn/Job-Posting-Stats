b17a255912b35e9e01887ef419d399175b586757
Sai Dixit Naidu <saidixitn@gmail.com>
2026-08-10 12:17:16 +0530

    Revert "Add configuration section for MongoDB and Telegram bot"
    This reverts commit c02846b9ecd34bf3bed2cb6e0afb9343d72156e9.

--- Alerts/posting_alerts.py
+++ Alerts/posting_alerts.py
@@ -5,9 +5,7 @@ import requests
 from concurrent.futures import ThreadPoolExecutor, TimeoutError as FutureTimeout
 
 # ============================================================
-
 # CONFIG
-
 # ============================================================
 LOCAL_MONGO_URI = os.getenv("LOCAL_MONGO_URI", "mongodb://localhost:27017/")
 BOT = os.getenv("TELEGRAM_BOT_TOKEN", "")
